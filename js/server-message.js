09cd3d7d28c99311d54e8721fb17036a9a041770
Compotation <46853874+Compotation@users.noreply.github.com>
2021-01-29 22:21:20 -0500

    formatting

--- js/server-message.js
+++ js/server-message.js
@@ -1,13 +1,12 @@
 function displayGreeting(greeting) {
-  let api_endpoint = "http://localhost:8080/greeting"
+  let api_endpoint = "http://localhost:8080/greeting";
 
   fetch(api_endpoint)
-    .then(response => response.text())
-    .then(response_text => greeting.textContent = response_text)
+    .then((response) => response.text())
+    .then((response_text) => (greeting.textContent = response_text));
 }
 
-
 window.addEventListener("DOMContentLoaded", (event) => {
-  const greeting = document.querySelector("#greeting")
-  displayGreeting(greeting)
-})
+  const greeting = document.querySelector("#greeting");
+  displayGreeting(greeting);
+});
