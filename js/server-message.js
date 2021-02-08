50c2ccc393bd2a15a1cef635fcaddd3f8613740f
Compotation <46853874+Compotation@users.noreply.github.com>
2021-02-08 16:20:36 -0500

    use strict mode and remove semi-colons

--- js/server-message.js
+++ js/server-message.js
@@ -1,12 +1,14 @@
+"use strict"
+
 function displayGreeting(greeting) {
-  let api_endpoint = "http://localhost:8080/greeting";
+  let api_endpoint = "http://localhost:8080/greeting"
 
   fetch(api_endpoint, { credentials: "include" })
     .then((response) => response.text())
-    .then((response_text) => (greeting.textContent = response_text));
+    .then((response_text) => (greeting.textContent = response_text))
 }
 
-window.addEventListener("DOMContentLoaded", (event) => {
-  const greeting = document.querySelector("#greeting");
-  displayGreeting(greeting);
-});
+window.addEventListener("DOMContentLoaded", () => {
+  const greeting = document.querySelector("#greeting")
+  displayGreeting(greeting)
+})
