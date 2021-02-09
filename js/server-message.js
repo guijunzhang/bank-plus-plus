fc55388bb150dd3dfcb032bc171c0a37ceddf477
Compotation <46853874+Compotation@users.noreply.github.com>
2021-02-09 09:28:54 -0500

    use new formatting style

--- js/server-message.js
+++ js/server-message.js
@@ -1,14 +1,14 @@
 "use strict"
 
 function displayGreeting(greeting) {
-  let api_endpoint = "http://localhost:8080/greeting"
+    let api_endpoint = "http://localhost:8080/greeting"
 
-  fetch(api_endpoint, { credentials: "include" })
-    .then((response) => response.text())
-    .then((response_text) => (greeting.textContent = response_text))
+    fetch(api_endpoint, { credentials: "include" })
+        .then((response) => response.text())
+        .then((response_text) => (greeting.textContent = response_text))
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  const greeting = document.querySelector("#greeting")
-  displayGreeting(greeting)
+    const greeting = document.querySelector("#greeting")
+    displayGreeting(greeting)
 })
